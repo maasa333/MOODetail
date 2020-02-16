72a738b16c7cbc8678723ed52a8e39f30212827c
maasa333 <konomaasa@gmail.com>
2020-02-16 14:45:26 -0800

    Form for noting new prompts to moods

--- moodetail-js-frontend/index.js
+++ moodetail-js-frontend/index.js
@@ -1,35 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // console.log("DOM content has loaded")
-    let url = "http://localhost:3000/moods";
-    fetch(url)
-    .then(resp => resp.json())
-    .then(data => data.map(mood => {
-        moodCard(mood);
-    }))
+    const moodUrl = "http://localhost:3000/moods";
+
+    // const fetchedMoods = [];
+
+    function fetchMoods() {
+        return fetch(moodUrl)
+            .then(resp => resp.json())
+            .then(data => data.map(mood => {
+                moodCard(mood);
+            }))
+    }
+    fetchMoods();
+
+    function moodCard(mood) {
+        const container = document.querySelector('#moods-container');
+        const moodCard = document.createElement('div')
+        moodCard.setAttribute('class', 'mood-card');
+        container.appendChild(moodCard);
+        moodCard.innerText = mood.state;
+        
+        addNotes(mood);
+    }
+
+    function addNotes(mood) {
+        const notes = mood.prompts.map(p => {
+            return p.desc;
+        })
+        const ul = document.createElement('ul');
+        const container = document.querySelector('#moods-container');
+        container.appendChild(ul);
+
+        notes.map(n => {
+            const li = document.createElement('li');
+            ul.appendChild(li)
+            return li.innerText = n
+        })
+    }
 })
-
-function moodCard(mood) {
-    const container = document.querySelector('#moods-container');
-    const moodCard = document.createElement('div')
-    moodCard.setAttribute('class', 'mood-card');
-    container.appendChild(moodCard);
-    moodCard.innerText = mood.state;
-    
-    addNotes(mood);
-}
-
-function addNotes(mood) {
-    const notes = mood.prompts.map(p => {
-        return p.desc;
-    })
-    const ul = document.createElement('ul');
-    const container = document.querySelector('#moods-container');
-    container.appendChild(ul);
-
-    notes.map(n => {
-        const li = document.createElement('li');
-        ul.appendChild(li)
-        return li.innerText = n
-    })
-}
-
