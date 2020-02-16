5955bd1c9ec1e1a61546389e279a71fe4cd3d5f0
maasa333 <konomaasa@gmail.com>
2020-02-16 14:01:27 -0800

    Append prompts to moods on dom

--- moodetail-api/config/routes.rb
+++ moodetail-api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'moods/index'
-  get 'moods/show'
+  get 'prompts/index'
+  get 'prompts/show'
+  resources :moods
+  resources :prompts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
