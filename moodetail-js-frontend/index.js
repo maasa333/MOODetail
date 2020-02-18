21fe4f4fae934816d8b0d9d86d00a4d9788181ea
maasa333 <konomaasa@gmail.com>
2020-02-17 18:52:45 -0800

    New prompts append to database and dom

--- moodetail-js-frontend/index.js
+++ moodetail-js-frontend/index.js
@@ -1,39 +1,90 @@
 document.addEventListener("DOMContentLoaded", () => {
     const moodUrl = "http://localhost:3000/moods";
+    const promptUrl  = "http://localhost:3000/prompts";
 
-    // const fetchedMoods = [];
+    const moodsObj = {moods: []};
+
+    fetchMoods();
 
     function fetchMoods() {
         return fetch(moodUrl)
-            .then(resp => resp.json())
-            .then(data => data.map(mood => {
-                moodCard(mood);
-            }))
+        .then(resp => resp.json())
+        .then(jsonData => {
+            for (const mood of jsonData) {
+                moodsObj.moods.push(mood);
+                createMoodCard(mood);
+            }
+        })
     }
-    fetchMoods();
 
-    function moodCard(mood) {
-        const container = document.querySelector('#moods-container');
-        const moodCard = document.createElement('div')
+    function createMoodCard(mood) {
+        const container = document.getElementById('moods-container');
+        const moodCard = document.createElement('div');
+        container.appendChild(moodCard);
         moodCard.setAttribute('class', 'mood-card');
+        moodCard.setAttribute('mood-id', mood.id)
         container.appendChild(moodCard);
         moodCard.innerText = mood.state;
         
-        addNotes(mood);
+        appendPrompts(mood);
     }
 
-    function addNotes(mood) {
-        const notes = mood.prompts.map(p => {
+    function appendPrompts(mood) {
+        const prompts = mood.prompts.map(p => {
             return p.desc;
         })
         const ul = document.createElement('ul');
         const container = document.querySelector('#moods-container');
         container.appendChild(ul);
 
-        notes.map(n => {
+        prompts.map(p => {
             const li = document.createElement('li');
-            ul.appendChild(li)
-            return li.innerText = n
+            ul.appendChild(li);
+            li.innerText = p;
         })
     }
-})
+
+    const formContainer = document.querySelector('div.form-container');
+    const inputs = document.querySelectorAll('.input-text')
+
+    formContainer.addEventListener('submit', e => {
+        e.preventDefault();        
+        const state = inputs[0].value;
+        const desc = inputs[1].elements[0].value;
+
+        const assignedMood = moodsObj.moods.find(mood => {
+            return mood.state === state
+        })
+        const moodId = assignedMood.id;
+        postPrompt(desc, moodId);
+    })
+
+    function postPrompt(desc, moodId) {
+        const data = {
+            desc: desc,
+            mood_id: moodId
+        }
+        return fetch(promptUrl, {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(resp => {
+            return resp.json()
+        })
+        .then(prompt => {
+            updatePrompts(prompt)
+        })
+    };
+
+    function updatePrompts(prompt) {
+        // debugger
+        const card = document.querySelector(`[mood-id = '${prompt.mood_id}']`)
+        const ul = card.nextSibling;
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerText = `${prompt.desc}`
+    }
+});
